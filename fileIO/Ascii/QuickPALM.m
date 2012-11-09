f0a3c05411e2e9eb27ef52152a7b382018484ae9
seamus.holden@gmail.com <seamus.holden@gmail.com@5fafa170-7d85-353c-40e8-82d88ad53326>
2012-11-09 15:30:24 +0000

    File import export for ascii files implemented. Tested on QuickPALM files. TODO: wire up to the gui TODO: rapidstorm import/export

---
+++ fileIO/Ascii/QuickPALM.m
@@ -0,0 +1,27 @@
+%;//File specification for QuickPALM
+fs.fileType = 'txt';
+fs.nHeaderLines = 1 ;
+fs.delimiter = '\t';
+fs.headerPrefix = ''; 
+fs.headerPostfix= ''; 
+fs.colNames={' ',
+   'Intensity',
+   'X (px)',
+   'Y (px)',  
+   'X (nm)',  
+   'Y (nm)',  
+   'Z (nm)',  
+   'Left-Width(px)'
+   'Right-Width (px)'
+   'Up-Height (px)'
+   'Down-Height (px)', 
+   'X Symmetry (%%)',   
+   'Y Symmetry (%%)',
+   'Width minus Height (px)',
+   'Frame Number'};
+%[OptionalColumnAssignments]
+fs.xCol='X (px)';
+fs.yCol='Y (px)';
+fs.zCol='Z (nm)';
+fs.frameCol='Frame Number';
+
---
+++ lib/testImportAscii.m
@@ -0,0 +1,9 @@
+fileSpec='QuickPALM.m';
+fname ='C:\Users\Holden.LEBPC15\Documents\data\121107PalmSieverTestData\QuickPALM.txt';
+fname2 ='C:\Users\Holden.LEBPC15\Documents\data\121107PalmSieverTestData\QuickPALMOut.txt';
+
+%varNames = fileIoAscii(fileSpec,'ReturnVarNames');
+%fileIoAscii(fileSpec,'Import','Filename',fname);
+fileIoAscii(fileSpec,'Export','Filename',fname2);
+%fileIoAscii(fileSpec,'Export','Filename',fname,'ColAssingment',colAssignHash);
+

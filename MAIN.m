017b295116c8dc0354bc0f7ff024242f0f48b3b3
thomas.pengo@gmail.com <thomas.pengo@gmail.com@5fafa170-7d85-353c-40e8-82d88ad53326>
2012-12-06 14:16:48 +0000

    circle default pointset. rendering min/max fixed.

--- MAIN.m
+++ MAIN.m
@@ -1,16 +1,18 @@
 % Main entry point
 setappdata(0,'usediplib',false)
 setappdata(0,'staticplugins',isdeployed)
+setappdata(0,'initialized',isdeployed)
 
-t = linspace(0,2*pi,1000)';
-X = sin(t)*1000;
-Y = cos(t)*1000;
+% t = linspace(0,2*pi,1000)';
+% X = sin(t)*1000;
+% Y = cos(t)*1000;
+% 
+% X = X+randn(1000,1)*20;
+% Y = Y+randn(1000,1)*20;
+% T = t;
+% Z = randn(1000,1)*20;
 
-X = X+randn(1000,1)*20;
-Y = Y+randn(1000,1)*20;
-Z = randn(1000,1)*20;
+% pluginsDir = fullfile(fileparts(which('palmsiever_setup')),'plugins');
 
-pluginsDir = fullfile(fileparts(which('palmsiever_setup')),'plugins');
-
-PALMsiever('X','Y')
+PALMsiever
 

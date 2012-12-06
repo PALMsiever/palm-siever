b8564a9d59a57cc9a3447d87ac4ef3e75fd94b93
thomas.pengo@gmail.com <thomas.pengo@gmail.com@5fafa170-7d85-353c-40e8-82d88ad53326>
2012-12-06 10:05:58 +0000

    Minor bug corrections, figure rearrangement, "Sieve" menu item.

--- lib/getVariables.m
+++ lib/getVariables.m
@@ -1,6 +1,7 @@
 function [rows2 data N] = getVariables(handles,N)
 
 if nargin<2
+    %warning('Refactor!');
     N = handles.settings.N;
 end
 
@@ -13,7 +14,7 @@ for irow=1:length(rows)
     var = evalin('base',rows{irow});
     % Skip logical vectors, vars beginning with '_', matrices, or vectors
     % of different length than N, or functions
-    if ~isnumeric(var) || rows{irow}(1)=='_' || size(var,2)>1 || nargin>1 && (size(var,1)~=N)
+    if ~isnumeric(var) || rows{irow}(1)=='_' || size(var,2)>1 || (nargin>1 && size(var,1)~=N)
         rowoffset=rowoffset+1;
         continue
     end
@@ -22,9 +23,17 @@ for irow=1:length(rows)
         try
             fev = evalin('base',[cols{icol} '(' rows{irow} ')']);
             if strcmp(cols{icol},'min')
-                fev = fev * .999;
+                if fev>=0
+                    fev = fev * .999;
+                else
+                    fev = fev / .999;
+                end
             elseif strcmp(cols{icol},'max')
-                fev = fev * 1.001;
+                if fev >=0
+                    fev = fev / .999;
+                else
+                    fev = fev * .999;
+                end
             end
             data{irow-rowoffset,icol} = fev;
         catch err

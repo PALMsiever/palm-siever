3432b966e40142df47d1ee6dcd2c27661743d626
seamus.holden@gmail.com <seamus.holden@gmail.com@5fafa170-7d85-353c-40e8-82d88ad53326>
2013-02-22 13:32:52 +0000

    Completely rewrote the fiducial drift correction plugin so that it actually works. Fits a spline to the raw data now (instead of an incredibly hamfisted interpolation + smoothing scheme).

---
+++ trunk/lib/combineAllDriftTracks.m
@@ -0,0 +1,83 @@
+function [xDrift, xAllCor] = combineAllDriftTracks(xAll,tUn)
+
+xDrift = NaN*ones(numel(tUn),1);
+xAllRaw = xAll;
+nPts = size(xAll,2);
+nTime = numel(tUn);
+lastGoodVal = 0;
+isLastValNaN = 1;
+for ii = 1:nTime
+   curRow = xAll(ii,:);
+   goodCol = find(~isnan(curRow));
+   if all(isnan(curRow))
+      xDrift(ii) = lastGoodVal;
+      isLastValNaN = 1;
+   elseif ~isLastValNaN % no need for offset correction
+      xDrift(ii) = mean(curRow(goodCol));
+      lastGoodVal = xDrift(ii);
+      isLastValNaN = 0;
+   else %last val is nan, need to correct offset
+      for jj = goodCol
+         xAll(:,jj) = xAll(:,jj) - xAll(ii,jj) + lastGoodVal;
+      end
+      xDrift(ii) = lastGoodVal;
+      isLastValNaN = 0;%ie next time we can just take the mean
+   end
+end
+      
+xAllCor = xAll;
+
+%
+%
+%
+%%%order the tracks by min timepoint
+%%colStartIsOk = zeros(1,nTr); %keep track of which columns have been corrected
+%%colEndIsOk = zeros(1,nTr); %keep 
+%%
+%%%do the starts
+%%while any(colStartIsOk==0)
+%%   [r,c,v] = findFirstGoodNum(xAll(:,~colStartIsOk));
+%%
+%%   if all(colStartIsOk==0)%ie if this is the first track to be corrected
+%%      corVal = 0;
+%%   else
+%%      corVal = mean(xAll(r,find(colStartIsOk)),2);
+%%      %need to check if corVal is NaN - if so interpolate from last known good value
+%%      if isnan(corVal)
+%%
+%%
+%%   end
+%%   xAll(:,c) = xAll(:,c) - xAll(r,c) + corVal;
+%%   xAll(1:r,c) = xAll(r,c);
+%%   colStartIsOk(c) = 1;
+%%end
+%%
+%%xDrift = xAll;
+%%
+%
+%%------------------------------------------------
+%function [r,c,v] = findFirstGoodNum(xAll)
+%nTr = size(xAll,2);
+%
+%r = Inf; c = 1;
+%for ii = 1:nTr
+%   [rI,cI] = find(~isnan(xAll(:,ii)),1,'first');
+%   if rI<r
+%      r = rI;
+%      c = ii;
+%   end
+%end
+%v = xAll(r,c);
+%%%------------------------------------------------
+%%function [r,c,v] = findLastGoodNum(xAll)
+%%nTr = size(xAll,2);
+%%
+%%r = Inf; c = 1;
+%%for ii = 1:nTr
+%%   [rI,cI] = find(~isnan(xAll(:,ii)),1,'last');
+%%   if rI<r
+%%      r = rI;
+%%      c = ii;
+%%   end
+%%end
+%%v = xAll(r,c);
---
+++ trunk/lib/correctDrift.m
@@ -0,0 +1,83 @@
+function [xc,yc,tDrift,xDrift,yDrift,zc,zDrift]  = correctDrift(driftTracks,nSpline,t, x,y, z)
+if isfield(driftTracks{1},'z')
+   isCorrectZ = true;
+else
+   isCorrectZ = false;
+end
+
+tUn = unique(sort(t));
+tDrift=tUn;
+
+%get cell of driftTracks from driftTracks
+if ~isCorrectZ
+   [xTr, yTr,tTr] = getTrackCell(driftTracks);
+else
+   [xTr, yTr,tTr,zTr] = getTrackCell(driftTracks);
+end
+
+%for each axis
+xDrift = getDrift(xTr,tTr,tUn,nSpline);
+xc = removeAxisDrift(x,t,xDrift,tUn);
+yDrift = getDrift(yTr,tTr,tUn,nSpline);
+yc = removeAxisDrift(y,t,yDrift,tUn);
+if isCorrectZ
+   zDrift = getDrift(zTr,tTr,tUn,nSpline);
+   zc = removeAxisDrift(z,t,zDrift,tUn);
+end
+
+%-----------------------------------------------------
+function xDrift = getDrift(xTr,tTr,tUn,nSpline)
+nPts = numel(xTr);
+nFrame = numel(tUn);
+xAll = zeros(nFrame,nPts);
+for ii = 1:numel(xTr)
+   xAll(:,ii) = getSplineToData(tTr{ii},xTr{ii},tUn,nSpline);
+end
+xDrift = combineAllDriftTracks(xAll,tUn);
+
+%-----------------------------------------------------
+function x = getSplineToData(tTr,xTr,tUn,nSpline)
+%returns NaN for tUn outside range of tTr
+
+xTr0=xTr;tTr0=tTr;
+%delete any nan's
+xtNaN = find(isnan(tTr) | isnan(xTr));
+tTr(xtNaN) =[];
+xTr(xtNaN) =[];
+%tidy up the vectors
+[tTr ix] = sort(tTr);
+xTr= xTr(ix);
+
+[tTr ia] = unique(tTr);
+xTr = xTr(ia);
+
+xDr =splinefit(tTr,xTr,nSpline,'r');
+xTrSpline = ppval(xDr,tTr);
+x = interp1(tTr,xTrSpline,tUn);
+%hold off;
+%plot(tTr,xTr);
+%hold all;
+%plot(tTr,xTrSpline);
+%plot(tTr,x);
+%pause
+
+
+%-----------------------------------------------------
+function xc = removeAxisDrift(x,t,xDrift,tUn);
+xc =x;
+for ii = 1:numel(tUn)
+   xc(t==tUn(ii)) = x(t==tUn(ii)) - xDrift(ii);
+end
+
+%-----------------------------------------------------
+function [xTr, yTr,tTr,zTr] = getTrackCell(driftTracks)
+
+for ii = 1:numel(driftTracks)
+   xTr{ii} = driftTracks{ii}.x;
+   yTr{ii} = driftTracks{ii}.y;
+   tTr{ii} = driftTracks{ii}.t;
+   if isfield(driftTracks{ii},'z')
+      zTr{ii} = driftTracks{ii}.z;
+   end
+end
+

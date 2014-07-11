6dbb31970ed40028e1937652ab58dfb8d7f8e87e
thomas.pengo@gmail.com <thomas.pengo@gmail.com@5fafa170-7d85-353c-40e8-82d88ad53326>
2014-07-11 10:12:45 +0000

    one line doc

--- trunk/lib/fetch.m
+++ trunk/lib/fetch.m
@@ -1,9 +1,10 @@
 % Fetch variables from the workspace by name
+function varargout = fetch(varargin)
+% Fetch variables from the workspace by name
 %
 %  varargin : list of variables to read
 %
 %  varargout : the requested variables
-function varargout = fetch(varargin)
 
 for i=1:nargin
     varargout{i} = evalin('base',varargin{i});

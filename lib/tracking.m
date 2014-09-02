239717bcef60b80554145cffa10557c7ca22b3f3
seamus.holden@gmail.com <seamus.holden@gmail.com@5fafa170-7d85-353c-40e8-82d88ad53326>
2014-09-02 13:43:29 +0000

    Added the Crocker and Grier tracking algorithm. Seems to give pretty similar results to current grouping alg in absence of diffusing molecules (finds similar # of tracks for FtsZ example dataset).

--- lib/tracking.m
+++ lib/tracking.m
@@ -1,4 +1,4 @@
-function grouping(handles)
+function tracking(handles)
 
 x = getX(handles);
 y = getY(handles);
@@ -7,8 +7,8 @@ subset = getSubset(handles);
 
 %%%%%%  READ IN PARAMETERS %%%%%%%
 
-prompt={'Maximum allowed gap between ''blinks'' [frames]: ','Maximum distance between two events [data units]:'};
-name='Grouping/merging multiple events';
+prompt={'Maximum gap (off-time) between localizations [frames]: ','Maximum distance between two events [data units]:'};
+name='Crocker/ Grier single particle tracking';
 numlines=1;
 defaultanswer={'0','50'};
 
@@ -27,59 +27,7 @@ if isnan(maxF) || isnan(maxD)
     return
 end
 
-%%%%%%%%%%%%%%
-%
-%maxD2 = maxD^2;
-%
-%if maxF>max(frame)
-%    maxF=0;
-%end
-%
-%t0 = cputime;
-%t00 = t0;
-%%cons is a linked list of points which are grouped
-%% contains addresses of other point within a track
-%cons = zeros(size(x));
-%for ff = min(frame):max(frame)-maxF
-%    ss = subset & frame>=ff & frame<=(ff+maxF);
-%    
-%    iss=find(ss)';
-%    
-%    xx = x(iss);
-%    yy = y(iss);
-%    
-%    %for all points
-%    for ii=1:length(iss)-1
-%        %find the nearest point
-%        [ mind2 imind2 ]= min((xx(ii)-xx(ii+1:end)).^2+(yy(ii)-yy(ii+1:end)).^2);
-%        %if its within the minimum radius, link it to the current point
-%        if mind2 < maxD2
-%            cons(iss(ii))=iss(ii+imind2);
-%        end
-%    end
-%    
-%    if cputime-t0 >1
-%       logger(sprintf('%d/%d frames done. ETA: %.2f secs',ff,max(frame)-maxF,(max(frame-maxF)-ff)/ff*(cputime-t00)));
-%       drawnow
-%       t0=cputime;
-%    end
-%end
-%
-%% Remove self
-%cons(cons'==(1:length(cons)))=0;
-%
-%% Calculate group frame IDs
-%N = size(cons,1);
-%curfg=1; group_frame_id=ones(N,1); group_id_=1:N;
-%while sum(group_frame_id==curfg)>0
-%    group_frame_id(cons(group_frame_id==curfg & cons>0))=group_frame_id(cons(group_frame_id==curfg & cons>0))+1;
-%    group_id_(cons(group_frame_id==curfg & cons>0))=group_id_(group_frame_id==curfg & cons>0);
-%    curfg=curfg+1;
-%end
-%
-%[zzz_ zzz_ group_id]= unique(group_id_);
-
-[group_id,group_frame_id] = gtrack_PSwrapper(x,y,frame,subset,maxF,maxD)
+[group_id,group_frame_id] = gtrack_PSwrapper(x,y,frame,subset,maxF,maxD);
 % Export
 assignin('base','group_frame_ID',group_frame_id);
 assignin('base','group_ID',group_id(:));
@@ -102,7 +50,7 @@ param.dim = 2;
 param.good= false;
 param.quiet = false;
 
-res = gtrack(xyt_subset,max_displacement,param);
+res = gtrack(xyit_subset,max_displacement,param);
 %FROM gtrack doc:
 % ;     
 % ;     For the input data structure (positionlist):
